48a7de8e20380c5886cfe599b54a8da366bd5070
ChristianCan-Ksquare <90230811+ChristianCan-Ksquare@users.noreply.github.com>
2022-06-12 22:44:27 -0500

    add screenshots functionality

--- js/script.js
+++ js/script.js
@@ -224,3 +224,24 @@ video.addEventListener("play", () => {
 video.addEventListener("pause", () => {
   videoContainer.classList.add("paused");
 });
+
+var canvas = document.querySelector("canvas");
+var context = canvas.getContext("2d");
+var w, h, ratio;
+//add loadedmetadata which will helps to identify video attributes......
+video.addEventListener(
+  "loadedmetadata",
+  function () {
+    ratio = video.videoWidth / video.videoHeight;
+    w = video.videoWidth - 100;
+    h = parseInt(w / ratio, 10);
+    canvas.width = w;
+    canvas.height = h;
+  },
+  false
+);
+///define a function
+function snap() {
+  context.fillRect(0, 0, w, h);
+  context.drawImage(video, 0, 0, w, h);
+}
